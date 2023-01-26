a161bdaac9035e0abffb376a3315eadce7651944
Federica Pozzer <federica.pozzer@gmail.com>
2023-01-26 18:00:14 +0100

    Dadi DONE (not pretty, but done!)

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -12,21 +12,38 @@
 // 5. se n = o pareggio --ritenta
 
 
-// 1. random numbers da 1 a 6 per il giocatore
-const random_number_1 = Math.floor(Math.random() * 6) + 1;
-console.log(random_number_1);
 
-// 2. random numbers da 1 a 6 per il computer
-const random_number_2 = Math.floor(Math.random() * 6) + 1;
-console.log(random_number_2);
+const my_button = document.querySelector(".real-button");
 
-// 3. se n > o vince n --stampa
-if (random_number_1 > random_number_2) {
-    console.log("Player-1 wins!");
-// 4. se o > n vince o --stampa
-} else if (random_number_1 < random_number_2) {
-    console.log("Player-2 wins!");
-// 5. se n = o pareggio --ritenta
-} else {
-    console.log("Pareggio! Ricarica la pagina per riprovare!");
-}
+my_button.addEventListener("click",
+    function () {
+        // 1. random numbers da 1 a 6 per il giocatore
+        const random_number_1 = Math.floor(Math.random() * 6) + 1;
+        // console.log(random_number_1);
+        document.getElementById("random-number-1").innerHTML = random_number_1;
+
+
+        // 2. random numbers da 1 a 6 per il computer
+        const random_number_2 = Math.floor(Math.random() * 6) + 1;
+        // console.log(random_number_2);
+        document.getElementById("random-number-2").innerHTML = random_number_2;
+
+
+        // 3. se n > o vince n --stampa
+        if (random_number_1 > random_number_2) {
+            // console.log("Player-1 wins!");
+            document.getElementById("the-winner-is").innerHTML = "Player 1 wins!";
+
+            // 4. se o > n vince o --stampa
+        } else if (random_number_1 < random_number_2) {
+            // console.log("Player-2 wins!");
+            document.getElementById("the-winner-is").innerHTML = "Player 2 wins!";
+
+            // 5. se n = o pareggio --ritenta
+        } else {
+            // console.log("Pareggio! Ricarica la pagina per riprovare!");
+            document.getElementById("the-winner-is").innerHTML = "Pareggio! Ricarica la pagina per riprovare!";
+        }
+    }
+    
+);
